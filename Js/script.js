0148913583679880c08ced00810d7489addc042e
madiinemm <uraimovamadina179@gmail.com>
2023-11-29 21:12:24 +0500

    masalalar yechildi

---
+++ Js/script.js
@@ -0,0 +1,73 @@
+ 1.// let a = 89;
+// let b = 76;
+// let res;
+// let max = (a / 10) - a % 10 / 10;
+// let max2 = (b / 10) - b % 10 / 10;
+// console.log(max, max2);
+// if (max > max2) {
+//     res = "katta"
+// } else {
+//     res = "kichik"
+// } console.log(res);
+
+ 2.// let a = 658
+// let max;
+// let b = a % 10;
+// let o = ((a % 100) - b) / 10;
+// let y = (a / 100) - (a % 100) / 100;
+
+// if (b > o) {
+//     max = b;
+// } else {
+//     max = o;
+// }
+// if (y > o) {
+//     max = o;
+// } else {
+//     max = y;
+// } console.log(max);
+
+ 3.// let a = 654;
+// let o = (a / 10) - a % 10 / 10;
+// if (o % 5 == 0) {
+//     res = "rost"
+// } else {
+//     res = "yolg'on"
+// } console.log(res);
+
+ 5.// let a = 38;
+// let b = 56;
+// let c = 65;
+// let d = 45;
+
+// let res;
+// if (a > b) {
+//     res = a;
+// } else {
+//     res = b;
+// } 
+// if (b > d) {
+//     res = d;
+// } else {
+//     res = b;
+// }
+// if (d > c) {
+//     res = d;
+// } else {
+//     res = c;
+// } console.log(res);
+
+ 6.// let a = 45;
+// let b = 23;
+// let c = 53;
+// let res;
+// if (b > c) {
+//     res = c;
+// } else {
+//     res = b;
+// }
+// if (b > a) {
+//     res = b;
+// } else {
+//     res = a
+// } console.log(res);
